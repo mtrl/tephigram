77317dfecc41efcbb66808f7da99daf58be0c54c
Mark Williams <oi.markwilliams@gmail.com>
2015-11-03 14:52:58 +0000

    Padding time with leading zero

--- get_skewt.py
+++ get_skewt.py
@@ -2,7 +2,7 @@
 import sys, urllib, os
 start_time = 800
 end_time = 2000
-image_dir = "images/{}/"
+image_dir = "images/day-{}/"
 url = 'http://rasp.inn.leedsmet.ac.uk/cgi-bin/get_rasp_skewt.cgi?region=UK%2b{}&grid=d2&day=0&lat=52.521483&lon=-2.877388&time={}'
 
 for day in range(0, 5):
@@ -10,7 +10,8 @@ for day in range(0, 5):
         os.makedirs(image_dir.format(day))
         print "Getting SkewT for day {}".format(day)
     for time in range(start_time, end_time, 100):
+        time = str(time).zfill(4)
         print "Downloading SkewT for day {} and time {}".format(day,time)
         skewt_image = urllib.URLopener()
         skewt_image.retrieve(url.format(day,time), image_dir.format(day)+"{}.png".format(time))
-        print "Downloaded!"
+print "Done"
