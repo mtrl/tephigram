14b6a452e39e9eb3f28658d4a05ae7dd3691b28b
Mark Williams <oi.markwilliams@gmail.com>
2015-11-04 15:07:01 +0000

    Set time step to hourly

--- scripts/get_skewt.py
+++ scripts/get_skewt.py
@@ -5,7 +5,7 @@ import sys, urllib, os, image
 num_days_to_dl = 5
 start_time = 700
 end_time = 1800
-time_step = 200
+time_step = 100
 url = 'http://rasp.inn.leedsmet.ac.uk/cgi-bin/get_rasp_skewt.cgi?region=UK{}&grid=d2&day=0&lat=52.521483&lon=-2.877388&time={}'
 
 image_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + "/../images") + "/{}/"
