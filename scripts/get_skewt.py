8fd4d2b6cacc9e1e384288b6d437377a9ffab52c
Mark Williams <oi.markwilliams@gmail.com>
2015-11-04 10:02:43 +0000

    Dir structure and python script updates

--- scripts/get_skewt.py
+++ scripts/get_skewt.py
@@ -2,26 +2,28 @@
 import sys, urllib, os
 #import sunrise
 # TODO: Set start and end time using a sunrise lib of some sort
+num_days_to_dl = 5
 start_time = 700
 end_time = 1800
 time_step = 200
-image_dir = "images/{}/"
 url = 'http://rasp.inn.leedsmet.ac.uk/cgi-bin/get_rasp_skewt.cgi?region=UK{}&grid=d2&day=0&lat=52.521483&lon=-2.877388&time={}'
 
-for day in range(0, 5):
+image_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + "/../images") + "/{}/"
+
+for day in range(0, num_days_to_dl + 1):
     if not os.path.exists(image_dir.format(day)):
         os.makedirs(image_dir.format(day))
         print "Getting SkewT for day {}".format(day)
     for time in range(start_time, end_time, time_step):
-        print time
+        time = str(time).zfill(4)
         if(day == 0):
             day_code = '4'
         else:
             day_code = '%2b' + str(day)
         image_url = url.format(day_code,time)
         #time = str(time).zfill(4)
-        print "Downloading SkewT for day {} and time {}".format(day,time)
-        print image_url
+        print "Downloading SkewT for day {} and time {}".format(day,str(time).zfill(4))
+        #print image_url
         try:
             image_file = image_dir.format(day)+"{}.png".format(time);
             skewt_image = urllib.URLopener()
