b9d974df7b01329ebff6c5426bb6eddd8e1eedc1
Mark Williams <oi.markwilliams@gmail.com>
2017-06-08 14:20:57 +0100

    Lots of changes to make the app work nicely when created as an app on iOS devices

--- blipspot.php
+++ blipspot.php
@@ -26,68 +26,11 @@ for($i = 0; $i <= 6; $i++)
     if($selected_day == date("l", $day)) {
         $day_buttons_html .= ' active';
     }
-    $day_buttons_html .= '" href="?day=' . date("l", $day) . '&tp=' . $tp . '">' . $the_date . '</a> ';
+    $day_buttons_html .= '" href="#" data-day="' . date('l', $day) .'">' . $the_date . '</a> ';
 }
 $day_buttons_html .= '</div>';
 
-// Get the image links
-$links = [
-    [
-        'title' => 'Star rating',
-        'url' => 'http://app.stratus.org.uk/blip/blip_stars.php?region=%s&tp=%s',
-        'info' => 'Note that the \'Stars\' Forecast is experimental and based on original work by Paul Scorer.'
-    ],
-    [
-        'title' => 'Thermal and Cloud',
-        'url' => 'http://app.stratus.org.uk/blip/blip_main.php?region=%s&tp=%s',
-        'info' => ''
-    ],
-    [
-        'title' => 'Cu Potential',
-        'url' => 'http://app.stratus.org.uk/blip/blip_cu.php?region=%s&tp=%s',
-        'info' => 'For Cu Potential, a positive number means there will be Cumulus clouds, a good day is typically around a 1000 (or more).'
-    ],
-    [
-        'title' => 'Dew Point and Dry Air Temperature',
-        'url' => 'http://app.stratus.org.uk/blip/blip_temp.php?region=%s&tp=%s',
-        'info' => 'On a good day, we need a 10 degree (or more) difference between the dry temperature and dew point.'
-    ],
-    [
-        'title' => 'Visible Surface Sun',
-        'url' => 'http://app.stratus.org.uk/blip/blip_sun.php?region=%s&tp=%s',
-        'info' => 'A sun percentage value of 60% or more means that the ground will warm up and clouds can develop. Less than 50% means there may be top cover or just too much cloud about (e.g. stratus "spread out" or Cirrus "top cover").'
-    ],
-    [
-        'title' => 'Boundary Layer Cloud Pc',
-        'url' => 'http://app.stratus.org.uk/blip/blip_get.php?day=%s&type=blcloudpct&tp=%s&output=plot',
-        'info' => 'Boundary layer cloud percent is useful to see if over developing. If the sun percent is low but the boundary layer percent is still high, check the Skew-T for top cover.'
-    ],
-    [
-        'title' => '1 Hour Accumulated Rain',
-        'url' => 'http://app.stratus.org.uk/blip/blip_rain.php?region=%s&tp=%s',
-        'info' => 'Ideally we do not want rain. A hint of a tenth of a millimetre means the air is fairly damp and the wet/dry temperatures will be close to each other.'
-    ],
-    [
-        'title' => 'Wind Speed',
-        'url' => 'http://app.stratus.org.uk/blip/blip_wind.php?region=%s&tp=%s',
-        'info' => ''
-    ]
-];
 
-$graph_html = "";
-
-foreach($links as $blip) {
-    $graph_html .= "<h2>{$blip['title']}";
-    if(!empty($blip['info'])) {
-        $div_id = str_replace(" ", "", $blip['title']);
-        $graph_html .= '<a class="glyphicon glyphicon-info-sign" data-toggle="collapse" data-target="#' . $div_id. '"></a>';
-        $graph_html .= '</h2>';
-        $graph_html .= '<p id="' . $div_id. '" class="collapse">' . $blip['info'] . '</p>';
-    } else {
-        $graph_html .= '</h2>';
-    }
-    $graph_html .= "<div><img src='".sprintf($blip['url'], $selected_day, $tp) . "' title=''></div>";
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -98,7 +41,6 @@ foreach($links as $blip) {
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <link rel="apple-touch-icon" href="blipspot-icon.png">
-
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Blipspots - Midland Gliding Club</title>
 
@@ -145,13 +87,13 @@ foreach($links as $blip) {
 <div class="container">
     <div class="row">
         <div class="col-lg-12 text-center">
-            <h1><?php echo $tp ?> Blipspots for <?php echo $selected_day ?></h1>
+            <h1><?php echo $tp ?> Blipspots</h1>
             <?php echo $day_buttons_html ?><br><br>
         </div>
     </div>
     <div class="row">
-        <div class="col-lg-12 text-center">
-            <?php echo $graph_html ?>
+        <div class="col-lg-12 text-center graphs">
+
         </div>
     </div>
 </div>
@@ -160,5 +102,87 @@ foreach($links as $blip) {
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+<script>
+    $(function() {
+        // Get the day
+        var selected_day = $('.btn.active').data('day');
+        updateBlips(selected_day);
+
+        $('.btn').on('click', function(el) {
+            console.log($(this).data('day'));
+            $('.btn.active').removeClass('active');
+            $(this).addClass('active');
+            // Get the day
+            var selected_day = $('.btn.active').data('day');
+            updateBlips(selected_day);
+            return false;
+        });
+
+        function updateBlips(selected_day) {
+            // Get the image links
+            var links =
+                [{
+                    'title' : 'Star rating',
+                    'url' : 'http://app.stratus.org.uk/blip/blip_stars.php?region={0}&tp={1}',
+                    'info' : 'Note that the \'Stars\' Forecast is experimental and based on original work by Paul Scorer.'
+                },
+                {
+                    'title' : 'Thermal and Cloud',
+                    'url' : 'http://app.stratus.org.uk/blip/blip_main.php?region={0}&tp={1}',
+                    'info' : ''
+                },
+                {
+                    'title' : 'Cu Potential',
+                    'url' : 'http://app.stratus.org.uk/blip/blip_cu.php?region={0}&tp={1}',
+                    'info' : 'For Cu Potential, a positive number means there will be Cumulus clouds, a good day is typically around a 1000 (or more).'
+                },
+                {
+                    'title' : 'Dew Point and Dry Air Temperature',
+                    'url' : 'http://app.stratus.org.uk/blip/blip_temp.php?region={0}&tp={1}',
+                    'info' : 'On a good day, we need a 10 degree (or more) difference between the dry temperature and dew point.'
+                },
+                {
+                    'title' : 'Visible Surface Sun',
+                    'url' : 'http://app.stratus.org.uk/blip/blip_sun.php?region={0}&tp={1}',
+                    'info' : 'A sun percentage value of 60% or more means that the ground will warm up and clouds can develop. Less than 50% means there may be top cover or just too much cloud about (e.g. stratus "spread out" or Cirrus "top cover").'
+                },
+                {
+                    'title' : 'Boundary Layer Cloud Pc',
+                    'url' : 'http://app.stratus.org.uk/blip/blip_get.php?day=%s&type=blcloudpct&tp={1}&output=plot',
+                    'info' : 'Boundary layer cloud percent is useful to see if over developing. If the sun percent is low but the boundary layer percent is still high, check the Skew-T for top cover.'
+                },
+                {
+                    'title' : '1 Hour Accumulated Rain',
+                    'url' : 'http://app.stratus.org.uk/blip/blip_rain.php?region={0}&tp={1}',
+                    'info' : 'Ideally we do not want rain. A hint of a tenth of a millimetre means the air is fairly damp and the wet/dry temperatures will be close to each other.'
+                },
+                {
+                    'title' : 'Wind Speed',
+                    'url' : 'http://app.stratus.org.uk/blip/blip_wind.php?region={0}&tp={1}',
+                    'info' : ''
+                }];
+
+            var graph_html = "";
+
+
+            $(links).each(function(i, blip) {
+//               console.log(el.title);
+                graph_html += "<h2>" + blip.title;
+                if(blip.info != '') {
+                    var div_id = blip.title.replace(" ", "");
+                    graph_html += '<a class="glyphicon glyphicon-info-sign" data-toggle="collapse" data-target="#' + div_id + '"></a>';
+                    graph_html += '</h2>';
+                    graph_html += '<p id="' + div_id + '" class="collapse">' + blip.info + '</p>';
+                } else {
+                    graph_html += '</h2>';
+                }
+                  graph_html += "<div><img src='" + blip.url.replace('{0}', selected_day).replace('{1}', '<?php echo $tp ?>') + "' title=''></div>";
+            });
+
+            $('.graphs').html(graph_html);
+        }
+    });
+</script>
+
 </body>
 </html>
