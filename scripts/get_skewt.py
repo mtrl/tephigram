ef7f5b4e65a63ba2d67d13598ca8d7bb8a3ed9e4
Mark Williams <oi.markwilliams@gmail.com>
2017-07-24 11:25:47 +0100

    Update to get_skewt script for new RASP URL

--- scripts/get_skewt.py
+++ scripts/get_skewt.py
@@ -7,7 +7,7 @@ lon = "-2.879209"
 start_time = 700
 end_time = 2000
 time_step = 100
-url = 'http://rasp.inn.leedsmet.ac.uk/cgi-bin/get_rasp_plot.cgi?region=UK{}&grid=d2&day=0&lat=' + lat + '&lon=' + lon + '&time={}&plot=skewt'
+url = 'http://rasp-uk.uk/cgi-bin/get_rasp_plot.cgi?region=UK{}&grid=d2&day=0&lat=' + lat + '&lon=' + lon + '&time={}&plot=skewt'
 image_dimensions = 800, 800
 
 image_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + "/../images") + "/{}/"
