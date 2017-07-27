6ed45c50730f1c37e55e62b6925b0b0167c30965
Mark Williams <oi.markwilliams@gmail.com>
2017-07-27 11:13:51 +0100

    More RASP UX tweaks

--- rasp/mobile.js
+++ rasp/mobile.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
    $('.selectorDiv-toggle').on('click', function() {
-      if($(this).hasClass('opened')) {
-          $(this).removeClass('opened')
-          $(this).find('.fa').removeClass('fa-angle-up').addClass('fa-angle-down').find('span').text('Show options');
+      if($(this).hasClass('open')) {
+          $(this).toggleClass('open').find('.fa').removeClass('fa-angle-up').addClass('fa-angle-down').find('span').text('Show options');
           $('#selectorDiv').hide();
       } else {
-          $(this).addClass('opened')
-          $(this).find('.fa').removeClass('fa-angle-down').addClass('fa-angle-up').find('span').text('Hide');
+          $(this).toggleClass('open').find('.fa').removeClass('fa-angle-down').addClass('fa-angle-up').find('span').text('Hide');
           $('#selectorDiv').show();
       }
    });
