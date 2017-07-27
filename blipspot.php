c20c7023c39beeeee808a604a0b02a220b2638ef
Mark Williams <oi.markwilliams@gmail.com>
2017-07-27 10:29:27 +0100

    Added wind dir to blipspots and moved wind speed below wind dir

--- blipspot.php
+++ blipspot.php
@@ -130,46 +130,53 @@ $day_buttons_html .= '</div>';
         function updateBlips(selected_day) {
             // Get the image links
             var links =
-                [{
-                    'title' : 'Star rating',
-                    'url' : 'http://app.stratus.org.uk/blip/blip_stars.php?region={0}&tp={1}',
-                    'info' : 'Note that the \'Stars\' Forecast is experimental and based on original work by Paul Scorer.'
-                },
-                {
-                    'title' : 'Thermal and Cloud',
-                    'url' : 'http://app.stratus.org.uk/blip/blip_main.php?region={0}&tp={1}',
-                    'info' : ''
-                },
-                {
-                    'title' : 'Cu Potential',
-                    'url' : 'http://app.stratus.org.uk/blip/blip_cu.php?region={0}&tp={1}',
-                    'info' : 'For Cu Potential, a positive number means there will be Cumulus clouds, a good day is typically around a 1000 (or more).'
-                },
-                {
-                    'title' : 'Dew Point and Dry Air Temperature',
-                    'url' : 'http://app.stratus.org.uk/blip/blip_temp.php?region={0}&tp={1}',
-                    'info' : 'On a good day, we need a 10 degree (or more) difference between the dry temperature and dew point.'
-                },
-                {
-                    'title' : 'Visible Surface Sun',
-                    'url' : 'http://app.stratus.org.uk/blip/blip_sun.php?region={0}&tp={1}',
-                    'info' : 'A sun percentage value of 60% or more means that the ground will warm up and clouds can develop. Less than 50% means there may be top cover or just too much cloud about (e.g. stratus "spread out" or Cirrus "top cover").'
-                },
-                {
-                    'title' : 'Boundary Layer Cloud Pc',
-                    'url' : 'http://app.stratus.org.uk/blip/blip_get.php?day={0}&type=blcloudpct&tp={1}&output=plot',
-                    'info' : 'Boundary layer cloud percent is useful to see if over developing. If the sun percent is low but the boundary layer percent is still high, check the Skew-T for top cover.'
-                },
-                {
-                    'title' : '1 Hour Accumulated Rain',
-                    'url' : 'http://app.stratus.org.uk/blip/blip_rain.php?region={0}&tp={1}',
-                    'info' : 'Ideally we do not want rain. A hint of a tenth of a millimetre means the air is fairly damp and the wet/dry temperatures will be close to each other.'
-                },
-                {
-                    'title' : 'Wind Speed',
-                    'url' : 'http://app.stratus.org.uk/blip/blip_wind.php?region={0}&tp={1}',
-                    'info' : ''
-                }];
+                [
+                    {
+                        'title' : 'Star rating',
+                        'url' : 'http://app.stratus.org.uk/blip/blip_stars.php?region={0}&tp={1}',
+                        'info' : 'Note that the \'Stars\' Forecast is experimental and based on original work by Paul Scorer.'
+                    },
+                    {
+                        'title' : 'Thermal and Cloud',
+                        'url' : 'http://app.stratus.org.uk/blip/blip_main.php?region={0}&tp={1}',
+                        'info' : ''
+                    },
+                    {
+                        'title' : 'Wind Direction',
+                        'url' : 'http://app.stratus.org.uk/blip/blip_wind_dir.php?region={0}&tp={1}',
+                        'info' : ''
+                    },
+                    {
+                        'title' : 'Wind Speed',
+                        'url' : 'http://app.stratus.org.uk/blip/blip_wind.php?region={0}&tp={1}',
+                        'info' : ''
+                    },
+                    {
+                        'title' : 'Cu Potential',
+                        'url' : 'http://app.stratus.org.uk/blip/blip_cu.php?region={0}&tp={1}',
+                        'info' : 'For Cu Potential, a positive number means there will be Cumulus clouds, a good day is typically around a 1000 (or more).'
+                    },
+                    {
+                        'title' : 'Dew Point and Dry Air Temperature',
+                        'url' : 'http://app.stratus.org.uk/blip/blip_temp.php?region={0}&tp={1}',
+                        'info' : 'On a good day, we need a 10 degree (or more) difference between the dry temperature and dew point.'
+                    },
+                    {
+                        'title' : 'Visible Surface Sun',
+                        'url' : 'http://app.stratus.org.uk/blip/blip_sun.php?region={0}&tp={1}',
+                        'info' : 'A sun percentage value of 60% or more means that the ground will warm up and clouds can develop. Less than 50% means there may be top cover or just too much cloud about (e.g. stratus "spread out" or Cirrus "top cover").'
+                    },
+                    {
+                        'title' : 'Boundary Layer Cloud Pc',
+                        'url' : 'http://app.stratus.org.uk/blip/blip_get.php?day={0}&type=blcloudpct&tp={1}&output=plot',
+                        'info' : 'Boundary layer cloud percent is useful to see if over developing. If the sun percent is low but the boundary layer percent is still high, check the Skew-T for top cover.'
+                    },
+                    {
+                        'title' : '1 Hour Accumulated Rain',
+                        'url' : 'http://app.stratus.org.uk/blip/blip_rain.php?region={0}&tp={1}',
+                        'info' : 'Ideally we do not want rain. A hint of a tenth of a millimetre means the air is fairly damp and the wet/dry temperatures will be close to each other.'
+                    }
+                ];
 
             var graph_html = "";
 
