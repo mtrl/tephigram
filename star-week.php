d270a66ebcb0275e63e31c15baa9a4dcdddd0bad
Mark Williams <oi.markwilliams@gmail.com>
2018-03-14 12:18:18 +0000

    Date format fix

--- star-week.php
+++ star-week.php
@@ -88,7 +88,7 @@ if ( isset( $_GET['tp'] ) ) {
 		$star_graph_html = '<div class="btn-group">';
 		for ( $i = 0; $i <= 6; $i ++ ) {
 			$day             = time() + ( $i * 86400 );
-			$the_date        = date( "D", $day );
+			$the_date        = date( "l", $day );
 			$star_graph_html .= '<div><br>';
 			$star_graph_html .= '<a href="blipspot.php?tp=' . $tp . '&day=' . $the_date . '"">';
 			$star_graph_html .= "<img src=\"http://app.stratus.org.uk/blip/blip_stars.php?region=" . date( 'l', $day ) . "&tp={$tp}\">";
