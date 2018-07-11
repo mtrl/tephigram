705bc843edb31ca0fb140809cf5bbbb352e02e74
Mark Williams <oi.markwilliams@gmail.com>
2018-07-11 15:07:28 +0100

    Added MGC ladder link to menu

--- partials/nav.php
+++ partials/nav.php
@@ -1,11 +1,12 @@
 <?php
 $buttons = array(
-	'UK STAR map'   => 'star-map.php',
+	'UK STAR Map'   => 'star-map.php',
 	'Mynd STAR Blipspots' => 'star-week.php',
 	'Mynd Tephigrams'     => 'tephigram.php',
 	'Mynd Blipspots'      => 'blipspot.php',
 	'RASPTable'  => 'rasp',
 	'Mynd Hangarcam'     => 'hangarcam.php',
+	'MGC Ladder Entries' => 'https://www.bgaladder.co.uk/Enquiry.asp?Season=2018&SortField=1&SortOrder=DESC&Month=0&SortField2=8&SortOrder2=DESC&Club=MID&PageSize=20&LPID=0&Glider=0&cboPilot=0&Action=Show+Results&cboTP=&iPage=1&PilotID=0&ckSortField=&ckSortField2=&ckSortOrder=&ckSortOrder2=&LO='
 );
 ?>
 
