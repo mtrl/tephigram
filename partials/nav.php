dbce1882e37d0a00f5bc7df8a05a090a9ae7123c
Mark Williams <oi.markwilliams@gmail.com>
2018-08-20 11:03:27 +0100

    Added wave forecast map

--- partials/nav.php
+++ partials/nav.php
@@ -1,16 +1,17 @@
 <?php
 $buttons = array(
-	'UK STAR Map'   => 'star-map.php',
-	'Mynd STAR Blipspots' => 'star-week.php',
+	'STAR Map'   => 'star-map.php',
+	'Wave Map'   => 'wave-forecast.php',
+	'Mynd STAR Blips' => 'star-week.php',
 	'Mynd Tephigrams'     => 'tephigram.php',
-	'Mynd Blipspots'      => 'blipspot.php',
+	'Mynd Blips'      => 'blipspot.php',
 	'RASPTable'  => 'rasp',
 	'Mynd Hangarcam'     => 'hangarcam.php',
 	'MGC Ladder Entries' => 'https://www.bgaladder.co.uk/Enquiry.asp?Season=2018&SortField=1&SortOrder=DESC&Month=0&SortField2=8&SortOrder2=DESC&Club=MID&PageSize=20&LPID=0&Glider=0&cboPilot=0&Action=Show+Results&cboTP=&iPage=1&PilotID=0&ckSortField=&ckSortField2=&ckSortOrder=&ckSortOrder2=&LO='
 );
 ?>
 
-<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+<nav class="navbar navbar-static-top" role="navigation">
 	<div class="container">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
