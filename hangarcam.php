0808739fb2baabcc756e137c001c24472f95b6ca
Mark Williams <oi.markwilliams@gmail.com>
2018-08-20 11:25:44 +0100

    Added simple loading text to hangarcam

--- hangarcam.php
+++ hangarcam.php
@@ -8,7 +8,8 @@ include_once ('partials/header.php');
 	<div class="row">
 		<div class="col-lg-12 text-center">
 			<h1>Latest Hangarcam</h1>
-			<img src="latest_hangarcam_jpeg.php" >
+			loading...
+			<img src="latest_hangarcam_jpeg.php">
 		</div>
 	</div>
 
